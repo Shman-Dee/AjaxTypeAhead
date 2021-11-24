f4b60b1022dc24e8a5649c2c40960991e3ace313
Andrew McInally <realfunnyestate@yahoo.com>
2021-11-23 23:27:06 -0700

    conquered

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
 const endpoint =
   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+let cities = [];
+
+const prom = fetch(endpoint)
+  .then((fu) => fu.json())
+  .then((data) => cities.push(...data));
+// console.log(cities);
+
+function findMatches(wordToMatch, cities) {
+  return cities.filter((place) => {
+    //check if the city or state matches what was searched
+    const regex = new RegExp(wordToMatch, "gi");
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray
+    .map((place) => {
+      const regex = new RegExp(this.value, "gi");
+      const cityName = place.city.replace(
+        regex,
+        `<span class="hl">${this.value}</span>`
+      );
+      const stateName = place.state.replace(
+        regex,
+        `<span class="hl">${this.value}</span>`
+      );
+      return `
+    <li>
+      <span class="name">${cityName}, ${stateName}</span>
+      <span class="population">${numberWithCommas(place.population)}</span>
+    </li>
+    `;
+    })
+    .join("");
+  suggestions.innerHTML = html;
+}
+
+const seachInput = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
+
+seachInput.addEventListener("change", displayMatches);
+seachInput.addEventListener("keyup", displayMatches);
